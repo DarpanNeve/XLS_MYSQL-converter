17bdde2d11da906da5386c2da6b5b7c9e6e55d31
darpan <darpanneve3@gmail.com>
2023-05-27 12:35:31 +0530

    new things added

--- Time_Table Sem_2.py
+++ Time_Table Sem_2.py
@@ -67,10 +67,8 @@ for count, day in enumerate(["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRID
                     j+=1
                 elif cv == "nan" or t == "nan" or cr == "nan":
                     cv = "Nan"
-                print(day + "     " + div + "     "+start+"     "+end + "     " + cv +
-                          "    " + batch+"     " + cr + "    " + t+"     "+type)
-                worksheet.append(
-                        [day, div, start, end, cv, batch, cr, t, type])
+            print(day + "     " + div + "     "+start+"     "+end + "     " + cv +"    " + batch+"     " + cr + "    " + t+"     "+type)
+            worksheet.append([day, div, start, end, cv, batch, cr, t, type])
             j += 1
         ascii += 1
         row += 1
