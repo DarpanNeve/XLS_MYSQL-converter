a37c536ecb3039d9ab3021ae1c9363b34f8cd647
= <=>
2023-11-14 22:45:21 +0530

    fixed indexing i think

--- Time_Table Sem_2.py
+++ Time_Table Sem_2.py
@@ -12,7 +12,7 @@ worksheet.append(["DAY", "DIVISION", "START", "END", "SUBJECT",
                  "BATCH", "CLASSROOM", "TEACHER", "TYPE"])
 
 # Load Excel file using pandas
-df = pd.read_excel('C:/Users/mywor/OneDrive/Desktop/programming/opensource/XLS_MYSQL-converter/Sem_2.xlsx',sheet_name='Final Copy')
+df = pd.read_excel('C:/Users/mywor/OneDrive/Desktop/programming/opensource/XLS_MYSQL-converter/Sem_2_old.xlsx', sheet_name='Final Copy')
 
 # Convert pandas DataFrame to numpy array
 data = np.array(df)
@@ -26,27 +26,27 @@ for count, day in enumerate(["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRID
         j = 1 if count == 1 else 12
         while j < (9 if count == 1 else 19):
             column = 1
-            cell_value = str(data[i][j])
-            teacher = str(data[i+1][j])
-            class_room = str(data[i+2][j])
-            timing = str(data[3][j]).replace(":", ".")
+            cell_value = str(data[i, j])
+            teacher = str(data[i + 1, j])
+            class_room = str(data[i + 2, j])
+            timing = str(data[3, j]).replace(":", ".")
             batch = "0"
             type = "T"
-            a, b = 1, 1
-            c = len(timing)
-            while b < len(timing):
-                if timing[b] == "-":
-                    start = timing[0:b]
-                    end = timing[b+1:c]
-                b = b+1
+            
+            # Handle the case where timing has multiple parts separated by "-"
+            if "-" in timing:
+                start, end = timing.split("-")
+            else:
+                start = timing
+                end = timing
 
             # Get the value of the next cell
-            next_cell_value = str(data[i][j+1])
+            next_cell_value = str(data[i, j + 1])
 
             # Check if the next cell is empty and the current cell is a practical
             if next_cell_value == "nan" and "/" in cell_value:
                 # If it is, update the end time to the end time of the next cell
-                next_timing = str(data[3][j+1]).replace(":", ".")
+                next_timing = str(data[3, j + 1]).replace(":", ".")
                 if "-" in next_timing:
                     end = next_timing.split("-")[1]
 
@@ -70,22 +70,22 @@ for count, day in enumerate(["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRID
             for cv, t, cr in zip(cell_values, teachers, class_rooms):
                 if cv == "*Incase of theory lecture it will end at 1:10 pm":
                     cv = "Lunch Break"
-                elif cv=="Life Skills":
-                    j+=1
+                elif cv == "Life Skills":
+                    j += 1
                 elif cv == "Theory lecture will end at 12:10pm":
                     cv = "Lunch Break"
                 elif "1" in cv:
-                    batch="1"
+                    batch = "1"
                 elif "2" in cv:
-                    batch="2"
+                    batch = "2"
                 elif "3" in cv:
-                    batch="3"
-                    j+=1
+                    batch = "3"
+                    j += 1
                 elif cv == "nan" or t == "nan" or cr == "nan":
                     cv = " "
-                    t=" "
-                    cr=" "
-                print(day + "     " + div + "     "+start+"     "+end + "     " + cv +"    " + batch+"     " + cr + "    " + t+"     "+type)
+                    t = " "
+                    cr = " "
+                print(day + "     " + div + "     " + start + "     " + end + "     " + cv + "    " + batch + "     " + cr + "    " + t + "     " + type)
                 worksheet.append([day, div, start, end, cv, batch, cr, t, type])
             j += 1
         ascii += 1
